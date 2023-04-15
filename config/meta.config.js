ed6ef5733cc46d19734131ac5045c7dad450b947
Shove1Bow1 <fireblaster2001@gmail.com>
2023-04-15 18:32:28 +0000

    update

--- config/meta.config.js
+++ config/meta.config.js
@@ -2,7 +2,7 @@ function responseMeta(message, status, err){
     const meta={  
         status: status?status:200,
         message: message?message:'success',
-        error: err?err:'',
+        error: err?err:'hello',
     }
     return{
         meta
