182187926bca6d282596d1e213aa767c09dc6ca3
Shove1Bow1 <fireblaster2001@gmail.com>
2023-05-09 20:59:42 +0700

    adding redis

--- src/routes/mobile-code.service.js
+++ src/routes/mobile-code.service.js
@@ -1,10 +1,19 @@
+const { responseMeta } = require('../../config/meta.config');
+const { responsePresenter } = require('../../config/reponse.config');
 const { getMobileCode } = require('../services/mobile-code.service');
+const router = require('./account.route');
 router.get("/:mobileId", [checkAuthorization], (req, res) => {
     try {
         const { mobileId } = req.param;
-        return res.status('200').send(getMobileCode(mobileId));
+        return res.status('200').send(
+            responsePresenter(
+                getMobileCode(getMobileCode),
+                responseMeta()
+            )
+        );
     }
     catch(error){
         return error;
     }
 })
+module.exports= router;
