a22696524bc055cf0ae97a72116cc64e5732ac4f
Trần Nguyễn Vĩnh Hoàng <Fireblaster2001@gmail.com>
2023-05-14 11:15:44 +0700

    Fix/merge nguyen branch (#11)
    * check spammer or potential-spammer
    
    * merge sucess

--- src/constant/value.js
+++ src/constant/value.js
@@ -1,9 +1,11 @@
-const POTENTIAL_SPAMMER=5;
-const SPAMMER=100;
+const POTENTIAL_SPAMMER=20;
+const SPAMMER=500;
+const CALLS_IN_MONTH=200;
 const LIST_STATUS=['unknown','potential-spammer','spammer']
 
 module.exports={
     POTENTIAL_SPAMMER,
     SPAMMER,
+    CALLS_IN_MONTH,
     LIST_STATUS
 }
