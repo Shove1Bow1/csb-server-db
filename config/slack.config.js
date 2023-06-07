30acc15246eb01639f4bf753435d65f379ae486d
Shove1Bow1 <fireblaster2001@gmail.com>
2023-06-07 20:43:28 +0700

    error log

--- config/slack.config.js
+++ config/slack.config.js
@@ -1,20 +1,31 @@
-const {WebClient}=require('@slack/web-api');
+const { WebClient } = require('@slack/web-api');
+const { logError } = require('./fs.config');
 
-const token=process.env.SLACK_TOKEN;
-const slackClient=new WebClient(token);
-function reportWithSlack(phoneNumber){
-    slackClient.chat.postMessage({
-        text:"Số điện thoại "+phoneNumber+" nhận 1 report.",
-        channel: "csb-notifications"
-    });
+const token = process.env.SLACK_TOKEN;
+const slackClient = new WebClient(token);
+function reportWithSlack(phoneNumber) {
+    try {
+        slackClient.chat.postMessage({
+            text: "Số điện thoại " + phoneNumber + " nhận 1 report.",
+            channel: "csb-notifications"
+        });
+    }
+    catch (error) {
+        logError(error);
+    }
 }
-function requestUnbanNumberWithSlack(phoneNumber,reason){
-    slackClient.chat.postMessage({
-        text:"Số điện thoại "+phoneNumber+" xin được hạ xuống potential-spammer.\nLý do: "+reason,
-        channel: "csb-unban"
-    })
+function requestUnbanNumberWithSlack(phoneNumber, reason, email) {
+    try {
+        slackClient.chat.postMessage({
+            text: "Email người gửi:" + (email || "unknown") + "\nSố điện thoại " + phoneNumber + " xin được hạ xuống potential-spammer.\nLý do: " + reason,
+            channel: "csb-unban"
+        })
+    }
+    catch (error) {
+        logError(error);
+    }
 }
-module.exports={
+module.exports = {
     reportWithSlack,
     requestUnbanNumberWithSlack
 }
